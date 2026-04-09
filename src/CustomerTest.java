f5aa4fb09b0ac5b471eb1ca47b8570126f433b55
Daniel Miuta <dan.miuta.24@lehre.mosbach.dhbw.de>
2026-04-09 21:50:00 +0200

    first commit also includes the unit tests

---
+++ src/CustomerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+class CustomerTest {
+
+    private Customer customer;
+    private String customerName;
+
+    @BeforeEach
+    void setUp() {
+        customerName = "John";
+        customer = new Customer(customerName);
+    }
+
+    @org.junit.jupiter.api.Test
+    void addRental() {
+        Rental rental = new Rental(new Movie("Test movie", 0), 3);
+        customer.addRental(rental);
+    }
+
+    @Test
+    void getName() {
+        Assertions.assertEquals("John", customer.getName());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void statement() {
+        Assertions.assertTrue(customer.statement().startsWith("Rental Record for John"));
+        Assertions.assertTrue(customer.statement().endsWith(" frequent renter points"));
+    }
+}
